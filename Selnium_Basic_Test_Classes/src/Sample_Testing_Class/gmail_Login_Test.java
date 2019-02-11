8b11dcfc06b796348dd1d83715d4fea56f90fdb3
anbu1525 <anbumaji@gmail.com>
2019-02-11 14:39:37 +0530

    password Encodeing

--- Selnium_Basic_Test_Classes/src/Sample_Testing_Class/gmail_Login_Test.java
+++ Selnium_Basic_Test_Classes/src/Sample_Testing_Class/gmail_Login_Test.java
@@ -1,4 +1,8 @@
 package Sample_Testing_Class;
+
+import java.util.Base64;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -6,67 +10,76 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class gmail_Login_Test {
+	
+	static String encodedPassword = "YmFudUAxNTI1";
+	
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.gecko.driver","/opt/Anbu/Azhagan/WorkSpace/Selenium_Drivers/NEW/geckodriver");
-	
-	//Initialize the instance
-	WebDriver driver = new FirefoxDriver();
+		System.setProperty("webdriver.gecko.driver", "/opt/Anbu/Azhagan/WorkSpace/Selenium_Drivers/NEW/geckodriver");
 
-	//launch gmail.com
-	driver.get("https://accounts.google.com");
+		// Initialize the instance
+		WebDriver driver = new FirefoxDriver();
 
-	//maximize the browser
-	driver.manage().window().maximize();
+		// launch gmail.com
+		driver.get("https://accounts.google.com");
 
-	//find the element by xpath and enter the email address which you want to login.
-	driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys("anbumaji");
+		// maximize the browser
+		driver.manage().window().maximize();
 
-	//wait for a seconds
-//	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		// find the element by xpath and enter the email address which you want to
+		// login.
+		driver.findElement(By.xpath("//input[@aria-label='Email or phone']")).sendKeys("anbumaji");
 
-	//find the Next Button and click on it.
-	driver.findElement(By.xpath("//*[text()='Next']/../..")).click();
+		// wait for a seconds
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+		// find the Next Button and click on it.
+		driver.findElement(By.xpath("//*[text()='Next']/../..")).click();
 
-    WebDriverWait wait = new WebDriverWait(driver, 100);
-    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='password']")));
-    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
+		WebDriverWait wait = new WebDriverWait(driver, 100);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@type='password']")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='password']")));
 
+		// find the password field and enter the password for the gmail.
+		driver.findElement(By.xpath("//*[@id='password']/div[1]/div/div[1]/input")).sendKeys(getDecodedPassword());
 
-	//find the password field and enter the password for the gmail.
-	driver.findElement(By.xpath("//*[@id='password']/div[1]/div/div[1]/input")).sendKeys("banu@1525");
-	
-	//*[@id="password"]/div[1]/div/div[1]/input
-	//find the Next Button and click on it.
-	driver.findElement(By.xpath("//*[text()='Next']/../..")).click();
+		// *[@id="password"]/div[1]/div/div[1]/input
+		// find the Next Button and click on it.
+		driver.findElement(By.xpath("//*[text()='Next']/../..")).click();
 
-	//get the hash code.
+		// get the hash code.
 //	int hashCodeValue = driver.GetHashCode();
 
-	//print the value.
+		// print the value.
 //	Console.WriteLine("Value is "+hashCodeValue);
 
-	//find the element by xpath n click.
+		// find the element by xpath n click.
 //	driver.findElement(By.xpath("(//a[@role='button'])[3]")).click();
+
+//		WebDriverWait wait1 = new WebDriverWait(driver, 100);
+//		wait1.until(ExpectedConditions.presenceOfElementLocated(
+//				By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
+//		wait1.until(ExpectedConditions.visibilityOfElementLocated(
+//				By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
+//
+//		// click on sign out.
+//		driver.findElement(By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")).click();
+//
+//		// wait for a second
+//		WebDriverWait wait11 = new WebDriverWait(driver, 100);
+//		wait11.until(ExpectedConditions.presenceOfElementLocated(
+//				By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
+//		wait11.until(ExpectedConditions.visibilityOfElementLocated(
+//				By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
+
+		Thread.sleep(3000);
+		driver.quit();
+
+	}
 	
-    WebDriverWait wait1 = new WebDriverWait(driver, 100);
-    wait1.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
-    wait1.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
+	public static String getDecodedPassword()
+	{
+		return new String(Base64.getDecoder().decode(encodedPassword.getBytes()));
+	}
+		
+	}
 
-	//click on sign out.
-	driver.findElement(By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")).click();
-
-	//wait for a second
-	 WebDriverWait wait11 = new WebDriverWait(driver, 100);
-	    wait11.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
-	    wait11.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div/header/div[2]/div[3]/div/div[2]/div/a/span")));
-
-
-	//close the driver instance.
-	driver.close();
-
-	//quit
-//	driver.quit();
-
-}
-}
